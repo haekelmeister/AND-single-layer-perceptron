6217240776840e500daae84b23afa72b6acf8b74
Häkelmeister <haekelmeister@gmail.com>
2018-02-12 10:59:00 +0100

    Implement most simplest AND ANN
    This implements the most simplest logical AND neural network I can imagine.
    
    How does it works? For each input vector we calculate the total
    stimulation for the output neuron. We let pass the input values in case
    it is at least that high as the input threshold (means the neuron was
    stimulated). Otherwise we set this to 0.
    
    The same logic applies to the output neuron. In case the total input
    (stimulation) reaches a certain level the neuron fires.

--- AND Artificial Neural Network/main.swift
+++ AND Artificial Neural Network/main.swift
@@ -1,9 +1,63 @@
 //
-//  main.swift
-//  AND Artificial Neural Network
+//  AND Single Layer Perceptron
 //
 
 import Foundation
 
-print("Hello, World!")
+/**
+ The activation function (or transfer function). It returns 0 or 1 in case the input
+ is higher than the given threshold.
+ */
+func neuronFires(threshold: Double, neuronInput: Double) -> Int {
+    if( neuronInput >= threshold ) {
+        return 1
+    }
 
+    return 0
+}
+
+    }
+    
+    return 0;
+}
+
+
+
+/**
+ This function demonstrates a single layer perceptron with logical AND.
+ No learning is applied, just the network calculation is made.
+ 
+ ```
+          +--------+
+  Input > | Neuron +-- 1 -+
+          +--------+      |
+                          v
+                     +--------+
+                     | Neuron | > Output
+                     +--------+
+                          ^
+          +--------+      |
+  Input > | Neuron +-- 1 -+
+          +--------+
+ ```
+ */
+func andNeuralNetwork() {
+    let inputVectors: [[Int]]       = [[0, 0], [0, 1], [1, 0], [1, 1]]
+    let connectionWeights: [Double] = [1, 1]
+    let THRESHOLD_OUTPUT_NEURON     = 1.5
+    let THRESHOLD_INPUT_NEURON      = 1.0
+    
+    for inputVector in inputVectors {
+        var outputNeuronNetworkInput = 0.0
+        for (input, weight) in zip(inputVector, connectionWeights) {
+            outputNeuronNetworkInput += Double(neuronFires(threshold: THRESHOLD_INPUT_NEURON, neuronInput: Double(input)))  * weight
+        }
+        
+        let neuronFired = neuronFires(threshold: THRESHOLD_OUTPUT_NEURON, neuronInput: outputNeuronNetworkInput)
+        print("\(inputVector) >> \(neuronFired)")
+    }
+}
+
+print("AND Single Layer Perceptron - Artificial Neural Network")
+
+andNeuralNetwork()
