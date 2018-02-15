62f4b34bf2c7385ac74c9980fe17b66f6a802646
Häkelmeister <haekelmeister@gmail.com>
2018-02-15 20:01:17 +0100

    Implement AND ANN with self learning
    This ANN works as the previuos one, but with the exception that the
    algorithm is able to adjust it's weights (to learn) in order to achieve
    the desired result. We use the so called DELTA rule here:
    
    delta W(i,k) = EPSILON * error rate * a(k)
    
    where:
    - EPSILON: the learning rate
    - error rate = activation level(target) - activation level(calculated)
    - a(k) = activation level input neuron

--- AND Artificial Neural Network/main.swift
+++ AND Artificial Neural Network/main.swift
@@ -16,9 +16,69 @@ func neuronFires(threshold: Double, neuronInput: Double) -> Int {
     return 0
 }
 
+/**
+ This function demonstrates a single layer perceptron with logical AND.
+ The connection weights will be adjusted by the DELTA rule:
+
+ Δw(ik) = 𝜀 * 𝛿(i) * a(k)
+
+ where:
+ - 𝜀 : the learning rate, mostly a constant
+ - 𝛿(i) : the error rate, desired output - calculated output
+ - a(k) : activation level of the corresponding input neuron
+
+ ```
+          +--------+
+ Input >  | Neuron +---- RANDOM
+          +--------+       |
+                           v
+                       +--------+
+                       | Neuron | > Output
+                       +--------+
+                           ^
+           +--------+      |
+ Input >   | Neuron +--- RANDOM
+           +--------+
+ ```
+*/
+func andNetworkWithLearningRules() {
+    let inputVectors: [[Int]]      = [[0, 0], [0, 1], [1, 0], [1, 1]]
+    let outputVector: [Int]        = [    0,      0,      0,      1]
+    var connectionWeights:[Double] = [drand48(), drand48()]
+//    var connectionWeights:[Double] = [-0.5, 0.5]
+    let THRESHOLD_OUTPUT_NEURON     = 1.5
+    let THRESHOLD_INPUT_NEURON      = 1.0
+    let EPSILON                     = 0.01
+
+    print("weights=\(connectionWeights)")
+
+    var run = true
+    var loopCounter = 0
+    while run {
+        loopCounter += 1
+        run = false
+        for (inputVector, output) in zip(inputVectors, outputVector) {
+            var outputNeuronStimulationLevel = 0.0
+            for (input, weight) in zip(inputVector, connectionWeights) {
+                outputNeuronStimulationLevel += Double(neuronFires(threshold: THRESHOLD_INPUT_NEURON, neuronInput: Double(input)))  * weight
+            }
+            let neuronFired = neuronFires(threshold: THRESHOLD_OUTPUT_NEURON, neuronInput: Double(outputNeuronStimulationLevel))
+
+            print("\(inputVector) >> \(neuronFired)")
+
+            if neuronFired != output {
+               // DELTA Rule: https://www.youtube.com/watch?v=4E4GhVREXBg
+                for index in 0..<inputVector.count {
+                    let deltaWeight = EPSILON * Double(output - neuronFired) * Double(neuronFires(threshold: THRESHOLD_INPUT_NEURON, neuronInput: Double(inputVector[index])))
+                    connectionWeights[index] += deltaWeight
+                }
+                run = true
+            }
+        }
+        print("weights=\(connectionWeights)")
     }
     
-    return 0;
+    print("Loops=\(loopCounter)")
 }
 
 
@@ -60,4 +120,5 @@ func andNeuralNetwork() {
 
 print("AND Single Layer Perceptron - Artificial Neural Network")
 
-andNeuralNetwork()
+//andNeuralNetwork()
+andNetworkWithLearningRules()
